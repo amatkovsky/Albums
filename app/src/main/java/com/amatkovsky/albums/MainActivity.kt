3a6156b4b8beb36fac1dd97c5815ed75b7759a48
Andrew Matkovsky <me@amatkovsky.com>
2023-05-18 06:36:19 -0400

    More deps

--- app/src/main/java/com/amatkovsky/albums/MainActivity.kt
+++ app/src/main/java/com/amatkovsky/albums/MainActivity.kt
@@ -3,13 +3,18 @@ package com.amatkovsky.albums
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.amatkovsky.albums.ui.theme.AlbumsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,21 @@ class MainActivity : ComponentActivity() {
             AlbumsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "greeting") {
+                        composable("greeting") {
+                            Column() {
+
+                                Greeting("Android")
+                                Button(onClick = { navController.navigate("screen2") }) {
+                                    Text("Nav")
+                                }
+                            }
+                        }
+                        composable("screen2") {
+                            Screen2()
+                        }
+                    }
                 }
             }
         }
@@ -32,6 +51,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun Screen2() {
+    Text(text = "Screen 2")
 }
 
 @Preview(showBackground = true)
